030323e54d9ffc8d3872969e76df8900085c0b29
AlexSorochinsky <alex.sorochinsky@gmail.com>
2016-11-19 12:51:55 +0300

    Try to increase casperjs timeouts

--- Server/Casper.js
+++ Server/Casper.js
@@ -11,7 +11,7 @@ BR.Casper = require('casper').create({
 
 BR.Casper.options.viewportSize = {width: 1920, height: 1080};
 
-BR.Casper.options.timeout = 100000;
+//BR.Casper.options.timeout = 100000;
 BR.Casper.options.retryTimeout = 100000;
 BR.Casper.options.waitTimeout = 100000;
 
